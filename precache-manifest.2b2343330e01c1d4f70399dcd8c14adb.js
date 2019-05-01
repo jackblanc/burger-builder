8db5d19962463027854bd6d0cf60d4d5c04cbaca
Jack Blanc <blanc.ja@husky.neu.edu>
2019-04-30 21:07:47 -0700

    Updates

---
+++ precache-manifest.2b2343330e01c1d4f70399dcd8c14adb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerbuilder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "9d7ef0e7a960638f7ec5",
+    "url": "/burgerbuilder/static/js/runtime~main.9d7ef0e7.js"
+  },
+  {
+    "revision": "757bba0255a5cccb1d33",
+    "url": "/burgerbuilder/static/js/main.757bba02.chunk.js"
+  },
+  {
+    "revision": "5a329877cca71cb2ab0d",
+    "url": "/burgerbuilder/static/js/1.5a329877.chunk.js"
+  },
+  {
+    "revision": "757bba0255a5cccb1d33",
+    "url": "/burgerbuilder/static/css/main.2b7e8ab3.chunk.css"
+  },
+  {
+    "revision": "a516a61799828ebd9c6b2c998c559533",
+    "url": "/burgerbuilder/index.html"
+  }
+];
